d56e0555ea6665dee9e3c1c2429768fcae9e099b
Mike Schroffel <imikes@gmail.com>
2019-11-05 11:08:51 -0800

    some shit

---
+++ email_process.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import re
+import os
+import sys
+import eml_parser
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', dest='eml_file',
+                        help='input file', required=True)
+options = parser.parse_args()
+
+if os.path.exists(options.eml_file):
+    with open(options.eml_file, 'rb') as f:
+        data = f.read()
+
+#eml_file = '/Users/mschroffel/Desktop/EDL stuffs..eml'
+
+
+parsed = eml_parser.eml_parser.decode_email_b(data, include_raw_body=True)
+
+eml_text = parsed['body'][0]['content']
+
+for line in eml_text.split('\n'):
+    m = re.search("[\d]{2}:[\d]{2}:[\d]{2}[^:]", line)
+    if m:
+        print("{}: ***** 01:{}".format(line, m.group(0)))
